95ae251c93843be95248dd8940078f08863db0af
kavidiuk <kaviduwarnakulasuriya@gmail.com>
2024-02-16 11:08:25 +0100

    Initial commit

---
+++ src/test/java/it/epicode/be/U5S2G5/U5S2G5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.be.U5S2G5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S2G5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
